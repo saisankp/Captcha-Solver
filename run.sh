f9a810972461fb01c43d468d5d718ceed983602b
Prathamesh Sai Sankar <saisankp@local.scss.tcd.ie>
2023-10-24 13:43:30 +0100

    changes

--- run.sh
+++ run.sh
@@ -2,8 +2,12 @@
 source env/bin/activate
 source .environment
 
-# 1. Data collection from server
+# 1. Data collection from server (run "ls | wc -l" in /individual-image to find amount of files downloaded, it should return 2000)
 python datacollection.py --url https://cs7ns1.scss.tcd.ie/?shortname=saisankp --output-directory individual-images
+
+#Cross checking from someone elses dataset
+python datacollection.py --url https://cs7ns1.scss.tcd.ie/?shortname=cheny28 --output-directory cross-check
+python datacollection.py --url https://cs7ns1.scss.tcd.ie/?shortname=corralp --output-directory cross-check2
 
 # 2. Training data generation
 python generation.py --width 128 --height 64 --length 5 --symbols symbols.txt --count 140800 --output-dir generated-images
