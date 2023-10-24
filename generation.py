f4984565dc5b00fbd2214e9cf0411cee04ebd411
hamzah7k <hamzahkhan7k@gmail.com>
2023-10-24 15:36:22 +0100

    Variable length

--- generation.py
+++ generation.py
@@ -7,19 +7,18 @@ import captcha.image
 from tqdm import tqdm
 import time
 
-# We don't want any duplicates in our training data and we need atleast 11 captchas generated since we split training vs validation data with 10/11 vs 1/11
-# If the count of captchas requested is greater than the number of possible unique captchas with size 'length', then exit
-def verifyCaptchaRequest(length, count):
-    # 26 uppercase letters, 26 lowercase letters, 10 digits
-    possibleCharacters = 26 + 26 + 10
-    uniqueCaptchasPossible = length ** possibleCharacters
-    if(count > uniqueCaptchasPossible):
-       print("Error: You are requesting " + str(count) + " images to be generated but only " + str(uniqueCaptchasPossible) + " images can have unique characters")
-       exit(1)
-    elif(count < 11):
+# We don't want any duplicates in our training data, and we need at least 11 captchas generated since we split training vs validation data with 10/11 vs 1/11.
+# If the count of captchas requested is greater than the number of possible unique captchas, then exit.
+def verifyCaptchaRequest(count):
+    # Determine the maximum number of unique captchas that can be generated for lengths 1 to 6
+    max_unique_captchas = sum([(26 + 26 + 10) ** length for length in range(1, 7)])
+
+    if count > max_unique_captchas:
+        print("Error: You are requesting more captchas than can be generated with lengths 1 to 6.")
+        exit(1)
+    elif count < 11:
         print("Error: Minimum number of captchas for training and validation images is 11")
         exit(1)
-
 
 # Parse arguments from the user to generate captchas
 def parseArguments(parser):
@@ -27,7 +26,6 @@ def parseArguments(parser):
     argumentsAndDescriptions = {
         '--width': ('Width of the generated captcha images', int),
         '--height': ('Height of the generated captcha images', int),
-        '--length': ('Number of characters in the generated captchas', int),
         '--count': ('Number of captchas to generate', int),
         '--output-dir': ('Directory to store the generated captchas', str),
         '--symbols': ('File which contains the symbol set for the generated captchas', str)
@@ -39,18 +37,17 @@ def parseArguments(parser):
     arguments = parser.parse_args()
 
     for argument, (description, _) in argumentsAndDescriptions.items():
-            if getattr(arguments, argument.replace("--", "").replace("-", "_")) is None:
-                print("Error: Please specify {}".format(argument))
-                exit(1)
+        if getattr(arguments, argument.replace("--", "").replace("-", "_")) is None:
+            print("Error: Please specify {}".format(argument))
+            exit(1)
 
-    verifyCaptchaRequest(arguments.length, arguments.count)
+    verifyCaptchaRequest(arguments.count)
 
     return arguments
 
-
 def main():
     start_time = time.time()
-    # Parse arguments and exit if any are msising
+    # Parse arguments and exit if any are missing
     arguments = parseArguments(argparse.ArgumentParser())
 
     # Create a captcha generator for the specified width and height of our required captchas
@@ -62,7 +59,7 @@ def main():
     symbols_file.close()
     print("Generating captchas with symbol set {" + captcha_symbols + "}")
 
-    # Creating folder where the generated captchas will go
+    # Creating a folder where the generated captchas will go
     if not os.path.exists(arguments.output_dir):
         print("Creating output directory " + arguments.output_dir)
         os.makedirs(arguments.output_dir)
@@ -80,16 +77,19 @@ def main():
     with open(os.path.join(trainingoutput_dir, 'output.txt'), 'w') as training_output_file, (
         open(os.path.join(validationoutput_dir, 'output.txt'), 'w')) as validation_output_file:
 
-        # Generate training and validation data with a split of 10/11 vs 1/11, same ratio to the example shown in the PDF given to us
+        # Generate training and validation data with a split of 10/11 vs 1/11, the same ratio as the example shown in the PDF given to us
         trainingdatasplit = arguments.count // 11 * 10
         for i in tqdm(range(arguments.count), desc="Generating Captchas"):
-            random_str = ''.join([random.choice(captcha_symbols) for j in range(arguments.length)])
+            # Choose a random length between 1 and 6
+            captcha_length = random.randint(1, 6)
+            random_str = ''.join([random.choice(captcha_symbols) for j in range(captcha_length)])
             image_path = os.path.join(training_dir if i < trainingdatasplit else validation_dir,
                                       random_str + '.png')
             
-            # We have randomly generated this captcha before, we need a new one.
+            # We have randomly generated this captcha before; we need a new one.
             while os.path.exists(image_path):
-                random_str = ''.join([random.choice(captcha_symbols) for j in range(arguments.length)])
+                captcha_length = random.randint(1, 6)
+                random_str = ''.join([random.choice(captcha_symbols) for j in range(captcha_length)])
                 image_path = os.path.join(training_dir if i < trainingdatasplit else validation_dir,
                                       random_str + '.png')
             
@@ -98,7 +98,7 @@ def main():
             cv2.imwrite(image_path, image)
             filename = os.path.basename(image_path)
 
-            # # Write the corresponding line for the captcha in the output file
+            # Write the corresponding line for the captcha in the output file
             output_file = training_output_file if i < arguments.count // 11 * 10 else validation_output_file
             output_file.write("{},{}\n".format(filename, random_str))
     end_time = time.time()
