0bb79b410225473c0aa96a31fb4d0b8e9228aae6
Prathamesh Sai <saisankp@tcd.ie>
2023-10-27 18:20:38 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,20 +2,20 @@
 source env/bin/activate
 source .environment
 
-# 1. Data collection from server, run "ls | wc -l" in folders to find amount of files downloaded, it should return 2000 (RUN USING RASPBERRY PI)
+# 1. Data collection from server, run "ls | wc -l" in folders to find amount of files downloaded, it should return 2000 (EXPERIMENT CONCLUSION: RUN USING RASPBERRY PI FOR THE BEST TIME)
 python datacollection.py --url https://cs7ns1.scss.tcd.ie/?shortname=saisankp --output-directory saisankp-images
 python datacollection.py --url https://cs7ns1.scss.tcd.ie/?shortname=hakhan --output-directory hakhan-images
 
-# 2. Generate captchas (RUN USING WSL)
+# 2. Generate captchas (EXPERIMENT CONCLUSION: RUN USING WSL FOR THE BEST TIME)
 python generation.py --width 128 --height 64 --min-length 1 --max-length 6 --symbols symbols_for_generation.txt --count 140800 --output-dir generated-images --font EamonU
 
-# 3. Process the generated captchas (RUN USING WSL)
+# 3. Process the generated captchas (EXPERIMENT CONCLUSION: RUN USING RUN USING WSL FOR THE BEST TIME)
 python process-images.py --image-folder generated-images/training/images --processed-image-folder processed-training-images
 python process-images.py --image-folder generated-images/validation/images --processed-image-folder processed-validation-images
 
-# 3. Train our model with the generated images (RUN USING WSL)
+# 3. Train our model with the generated images (EXPERIMENT CONCLUSION: RUN USING WSL FOR ENOUGH TIME TO TRAIN THE MODEL)
 python train.py --width 128 --height 64 --length 6 --batch-size 32 --train-dataset processed-training-images --validate-dataset processed-validation-images --output-model-name model.h5 --symbols symbols.txt --epochs 5
 
-# 4. Classify the captchas to get our Submitty file (RUN USING RASPBERRY PI)
+# 4. Classify the captchas to get our Submitty file (EXPERIMENT CONCLUSION: RUN USING RASPBERRY PI FOR THE BEST TIME)
 python classify.py --model-name model --captcha-dir saisankp-images --output saisankp.csv --symbols symbols.txt
 python classify.py --model-name model --captcha-dir hakhan-images --output hakhan.csv --symbols symbols.txt
