f46efcf552804530bde8c2a1381aa30231088cd2
Prathamesh Sai <saisankp@tcd.ie>
2023-10-27 18:52:31 +0100

    Update classify.py

--- classify.py
+++ classify.py
@@ -2,7 +2,6 @@ import warnings
 import time
 warnings.filterwarnings("ignore", category=FutureWarning)
 warnings.filterwarnings("ignore", category=DeprecationWarning)
-
 import os
 import cv2
 import numpy
@@ -10,71 +9,72 @@ import string
 import random
 import argparse
 import tensorflow as tf
-import tensorflow.keras as keras
+from tensorflow import keras
+
+
+def parseArguments(parser):
+    parser = argparse.ArgumentParser()
+    argumentsAndDescriptions = {
+    '--width': ('Width of the generated captcha images', int),
+    '--model-name': ('Model name to use for classification', str),
+    '--captcha-dir': ('Where to read the captchas from', str),
+    '--output': ('File where the classifications should be saved', str),
+    '--symbols': ('File with the symbols to use in captchas', str)
+    }
+
+    for argument, (description, argument_type) in argumentsAndDescriptions.items():
+        parser.add_argument(argument, help=description, type=argument_type)
+
+    arguments = parser.parse_args()
+
+    for argument, (description, _) in argumentsAndDescriptions.items():
+        if getattr(arguments, argument.replace("--", "").replace("-", "_")) is None:
+            print("Error: Please specify {}".format(argument))
+            exit(1)
+
+    return arguments
+
 
 def decode(characters, y):
     y = numpy.argmax(numpy.array(y), axis=2)[:,0]
     return ''.join([characters[x] for x in y])
 
+
 def main():
     start_time = time.time()
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--model-name', help='Model name to use for classification', type=str)
-    parser.add_argument('--captcha-dir', help='Where to read the captchas to break', type=str)
-    parser.add_argument('--output', help='File where the classifications should be saved', type=str)
-    parser.add_argument('--symbols', help='File with the symbols to use in captchas', type=str)
-    args = parser.parse_args()
-
-    if args.model_name is None:
-        print("Please specify the CNN model to use")
-        exit(1)
-
-    if args.captcha_dir is None:
-        print("Please specify the directory with captchas to break")
-        exit(1)
-
-    if args.output is None:
-        print("Please specify the path to the output file")
-        exit(1)
-
-    if args.symbols is None:
-        print("Please specify the captcha symbols file")
-        exit(1)
-
-    symbols_file = open(args.symbols, 'r')
+    # Parse arguments and exit if any are missing
+    arguments = parseArguments(argparse.ArgumentParser())
+    symbols_file = open(arguments.symbols, 'r')
     captcha_symbols = symbols_file.readline().strip()
     symbols_file.close()
-
     print("Classifying captchas with symbol set {" + captcha_symbols + "}")
-
+    # Only using CPU because of limited throttled computational power on the Raspberry Pi
     with tf.device('/cpu:0'):
-        with open(args.output, 'w') as output_file:
-            json_file = open(args.model_name+'.json', 'r')
+        with open(arguments.output, 'w') as output_file:
+            json_file = open(arguments.model_name+'.json', 'r')
             loaded_model_json = json_file.read()
             json_file.close()
             model = keras.models.model_from_json(loaded_model_json)
-            model.load_weights(args.model_name+'.h5')
+            model.load_weights(arguments.model_name+'.h5')
             model.compile(loss='categorical_crossentropy',
                           optimizer=keras.optimizers.Adam(1e-3, amsgrad=True),
                           metrics=['accuracy'])
-
-            for x in os.listdir(args.captcha_dir):
-                # load image and preprocess it
-                raw_data = cv2.imread(os.path.join(args.captcha_dir, x))
+            for x in sorted(os.listdir(arguments.captcha_dir)):
+                raw_data = cv2.imread(os.path.join(arguments.captcha_dir, x))
                 rgb_data = cv2.cvtColor(raw_data, cv2.COLOR_BGR2RGB)
                 image = numpy.array(rgb_data) / 255.0
                 (c, h, w) = image.shape
                 image = image.reshape([-1, c, h, w])
                 prediction = model.predict(image)
                 output_file.write(x + "," + decode(captcha_symbols, prediction).replace("$","") + "\n")
-
-                print('Classified ' + x)
+                print('Classified: ' + x)
+                print('The model predicts: ' + decode(captcha_symbols, prediction).replace("$","") )
     end_time = time.time()
     time_taken = end_time - start_time
-
     with open('classify_time_taken.txt', 'w') as file:
         file.write('Classify: {:.2f} seconds'.format(time_taken))
         print('Time taken for classifying captchas from server:', time_taken, 'seconds')
+
 
 if __name__ == '__main__':
     main()
